dbdbc55c5b431e2aed6e69d57d810cfc1f651f9b
dkoo <derrick_koo@newyorker.com>
2015-11-22 14:31:33 -0500

    clean up user.js

--- server/js/admin/user.js
+++ server/js/admin/user.js
@@ -1,13 +1,3 @@
-Meteor.startup(function() {
-	var dkoo = Meteor.users.findOne( { username: 'dkoo' } );
-
-	Meteor.users.update(dkoo._id, {
-		$set: {
-			'profile.interests': ['guitar', 'songwriting', 'audio production', 'psychedelic rock', 'running', 'hiking', 'biking', 'skiing', 'goju-ryu karate']
-		}
-	});
-});
-
 Meteor.dkoo = {
 	checkUser: function(userId) {
 		var dkoo = Meteor.users.findOne( { username: 'dkoo' } );
