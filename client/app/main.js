b3179908b340f897131a1ef6640c00010c2740e8
dkoo <derrick_koo@newyorker.com>
2016-02-29 23:06:08 -0500

    add back basic <meta> tags

--- client/app/main.js
+++ client/app/main.js
@@ -14,28 +14,4 @@ Meteor.startup(function() {
 			}
 		}
 	});
-
-	// default SEO title/meta tags
-	// return SEO.config({
-	// 	title: 'dkoo dot net',
-	// 	meta: {
-	// 		'description': 'Personal website of Derrick Koo, a Brooklyn-based writer and developer.'
-	// 	},
-	// 	og: {
-	// 		'title': 'dkoo dot net',
-	// 		'description': 'Personal website of Derrick Koo, a Brooklyn-based writer and developer.',
-	// 		'site_name': 'dkoo dot net',
-	// 		'url': 'http://dkoo.net',
-	// 		'image': 'http://dkoo.net/uploads/images/2230142.jpg'
-	// 	},
-	// 	twitter: {
-	// 		'title': 'dkoo dot net',
-	// 		'card': 'summary',
-	// 		'description': 'Personal website of Derrick Koo, a Brooklyn-based writer and developer.',
-	// 		'site_name': 'dkoo dot net',
-	// 		'site': '@derrick_koo',
-	// 		'image': 'http://dkoo.net/uploads/images/2230142.jpg'
-
-	// 	}
-	// });
 });
